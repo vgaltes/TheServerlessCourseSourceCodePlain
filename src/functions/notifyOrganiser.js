09fef2c87e7ff3ef3eb09088b32001c9756f554b
Vicenç García Altés <vgales@gmail.com>
2021-03-04 15:19:35 +0000

    Lesson 14

---
+++ src/functions/notifyOrganiser.js
@@ -0,0 +1,8 @@
+module.exports.handler = async (event, context) => {
+    const joinedGetTogether = JSON.parse(event.Records[0].Sns.Message);
+  
+    console.log(`notified organiser [${joinedGetTogether.getTogetherId}, ${joinedGetTogether.orderId}, ${joinedGetTogether.userEmail}]`);
+  
+    return "all done";
+  };
+  

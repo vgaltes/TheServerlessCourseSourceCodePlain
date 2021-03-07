b89362f6589f72e5fdefcaeef6b6262e652cedee
Vicenç García Altés <vgales@gmail.com>
2021-03-07 10:27:28 +0000

    Lesson 20

--- src/functions/notifyOrganiser.js
+++ src/functions/notifyOrganiser.js
@@ -2,12 +2,9 @@ const Log = require('@dazn/lambda-powertools-logger');
 const middy = require("middy");
 const correlationIds = require('@dazn/lambda-powertools-middleware-correlation-ids');
 
+
 const handler = async (event, context) => {
   const joinedGetTogether = JSON.parse(event.Records[0].Sns.Message);
-
-  if (joinedGetTogether.getTogetherId === "error"){
-    throw new Error("Simulating error");
-  }
 
   Log.info('notified organiser', {
     getTogetherId: joinedGetTogether.getTogetherId, 

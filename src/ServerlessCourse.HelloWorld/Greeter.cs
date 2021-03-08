e24902c91acdeb2fc16c25364a702a46713434de
Vicenç García <me@vgaltes.com>
2021-03-08 16:42:50 +0000

    Net core with Middy Net

--- src/ServerlessCourse.HelloWorld/Greeter.cs
+++ src/ServerlessCourse.HelloWorld/Greeter.cs
@@ -2,21 +2,35 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 using System.Text.Json;
+using Voxel.MiddyNet;
+using Voxel.MiddyNet.SSMMiddleware;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace ServerlessCourse.HelloWorld
 {
-    public class Greeter
+    public class Greeter : MiddyNet<APIGatewayProxyRequest, APIGatewayProxyResponse>
     {
-        public Task<APIGatewayProxyResponse> Handler(APIGatewayProxyRequest request, ILambdaContext context)
+        public Greeter()
         {
-            context.Logger.Log(JsonSerializer.Serialize(request));
-
+            Use(new SSMMiddleware<APIGatewayProxyRequest, APIGatewayProxyResponse>( new SSMOptions{
+                CacheExpiryInMillis = 60000,
+                ParametersToGet = new System.Collections.Generic.List<SSMParameterToGet>{
+                    new SSMParameterToGet("TableName", "/gettogethers/dev/secureTableName")
+                }
+            }));
+        }
+        protected override Task<APIGatewayProxyResponse> Handle(APIGatewayProxyRequest request, MiddyNetContext context)
+        {
             var name = request.QueryStringParameters["name"];
 
             var body = new {
                 message = $"hello {name}"
             };
+
+            context.Logger.EnrichWith(new LogProperty("propiedadExtra", name));
+            context.Logger.EnrichWith(new LogProperty("tableName", 
+                context.AdditionalContext["TableName"]));
+            context.Logger.Log(LogLevel.Info, "estamos dentro");
 
             var result = new APIGatewayProxyResponse
             {

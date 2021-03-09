1d27629625b1cb335fae6108792393721ea04955
Vicenç García Altés <vgales@gmail.com>
2021-03-09 22:48:13 +0000

    Lesson 27

--- src/functions/cognitoPrivate.js
+++ src/functions/cognitoPrivate.js
@@ -1,6 +1,10 @@
 module.exports.handler = async event => {
     const res = {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
       body: JSON.stringify(`Hello from a private enpoint`)
     };
   
